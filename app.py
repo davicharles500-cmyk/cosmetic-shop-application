4979a0d3cafa30bbab07540caa4bd0e5cefe00f4
davicharles500-cmyk <davicharles500@gmail.com>
2026-02-16 20:01:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -158,4 +158,3 @@ def add_product():
 # ==================== MAIN ====================
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
-
